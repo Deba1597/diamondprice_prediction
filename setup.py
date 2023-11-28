7aec3b944c5dbee295b76e2491a3b12d1073b4ac
Deba1597 <jbdebasishbehera@gmail.com>
2023-11-28 14:36:00 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'DiamondPrice',
+    version= '0.0.1',
+    author='Debasish',
+    author_email='jbdebasishbehera@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
